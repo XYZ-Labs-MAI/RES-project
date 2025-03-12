6e5a31747f5b6ed624edfffaeca22645e9c89ebc
ZeroXM <bertim1243@gmail.com>
2025-03-12 17:52:35 +0300

    small fixes in static files

--- app/XYZ_labs/settings.py
+++ app/XYZ_labs/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ 
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'web/staticfiles',
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- app/home_page/views.py
+++ app/home_page/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home_page_show(request):
-    return render(request, 'home_page/home_page.html')
+    return render(request, 'home_page/general_page.html')
 
 
 @login_required
